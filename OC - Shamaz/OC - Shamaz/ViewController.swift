47c198e2d8623b724baba471df72b4bacad6c2a3
Leo <leo.a.rubiano@gmail.com>
2018-08-05 21:28:34 -0500

    brap

--- OC - Shamaz/OC - Shamaz/ViewController.swift
+++ OC - Shamaz/OC - Shamaz/ViewController.swift
@@ -12,46 +12,76 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        resetUI()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        storyPrompt.text = "";
+        nextPlayer.text = "";
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    enum PromptTense {
+        case future
+        case past
+    }
 
-
+    //Past tense options
+    let pastTensePrompts = ["Describe a time when you felt happy",
+                            "Tell us about a time when you felt scared",
+                            "Tell us about an occasion when you felt embarrassed",
+                            ]
+    
+    //Future tense options
+    let futureTensePrompts = ["Where would you like to live in",
+                              "What would you like to be doing for a living in",
+                              "How often would you like to see your friends in"
+                              ]
+    
+    let timeIntervalNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+    let timeIntervalUnits = ["day", "week", "month", "year"]
+    
     @IBOutlet weak var storyPrompt: UILabel!
     
     @IBOutlet weak var nextPlayer: UILabel!
     
     @IBAction func promptPastAction(_ sender: UIButton) {
-        storyPrompt.text = generatePastAction()
+        storyPrompt.text = generatePrompt(tense: PromptTense.past)
     }
     
     @IBAction func promptFutureAction(_ sender: UIButton) {
-        storyPrompt.text = generateFutureAction()
+        storyPrompt.text = generatePrompt(tense: PromptTense.future)
     }
     
     @IBAction func selectNextPlayer(_ sender: UIButton) {
         //return random number from range of 1-10
         nextPlayer.text = String(arc4random_uniform(10) + 1)
     }
-
-    func resetUI(){
-        storyPrompt.text = "";
-        nextPlayer.text = "";
+    
+    func generatePrompt(tense: PromptTense)-> String {
+        if tense == PromptTense.future {
+            return generateFutureAction()
+        } else {
+            return generatePastAction()
+        }
     }
     
     func generatePastAction() -> String {
+        var prompt: String = ""
+        //generate Past tense prompt
         
-        return "past action"
+        
+        return prompt
     }
     
     func generateFutureAction() -> String {
+        var prompt: String = ""
         
-        return "future action"
+        //generate Future tense prompt
+        
+        return prompt
     }
     
 }
